f38808dbe7994aea1974c3c2202b335c96cb9b47
Mochamad Gufron <mgufronefendi@gmail.com>
2013-07-31 09:08:29 +0700

    create pdftohtml library

---
+++ src/PdfToHtml.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Gufy;
+
+class PdfToHtml
+{
+	const OPTIONS=array(
+
+	);
+	public function open($pdfFile)
+	{
+
+	}
+	public function generate()
+	{
+
+	}
+	public function setOptions()
+	{
+
+	}
+}
---
+++ tests/PdfToHtmlTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Gufy\PdfToHtml;
+
+class PdfToHtmlTest extends \PHPUnit_Framework_TestCase
+{
+	public $bin;
+	public function setUp()
+	{
+		$this->bin = new PdfToHtml;
+	}
+	public function testOpen()
+	{
+		$file = __DIR__.'/source/test.pdf';
+		$this->assertTrue(file_exists($file));
+		$this->bin->open($file);
+	}
+	public function testOpenAndGenerate()
+	{
+
+		$file = __DIR__.'/source/test.pdf';
+		$this->assertTrue(file_exists($file));
+		$this->bin->open($file);
+		$this->bin->setOutputDirectory(__DIR__.'/results');
+		
+		$this->bin->generate();
+		$this->assertTrue(count(scandir(__DIR__.'/results'))>=3);
+
+		$this->bin->clearOutputDirectory();
+	}
+	public function testGenerateSinglePage()
+	{
+		$file = __DIR__.'/source/test.pdf';
+		$this->assertTrue(file_exists($file));
+		$this->bin->open($file);
+		$this->bin->setOutputDirectory(__DIR__.'/results');
+		
+		$this->bin->setOptions("singlePage",true);
+		$this->bin->generate();
+		$this->assertTrue(count(scandir(__DIR__.'/results'))>=3);
+
+		$this->bin->clearOutputDirectory();
+	}}
